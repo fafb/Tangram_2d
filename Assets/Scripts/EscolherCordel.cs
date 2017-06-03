4d8acd0f88cf50de165b26b6e10faa09c7050799
alyssonbezerra <fafb.alysson@gmail.com>
2017-06-03 13:59:42 -0300

    Implementado a ação de iniciar o jogo, esolhar o cordel desejado e iniciar o jogor com o primeiro tangram do cordel desejado.

---
+++ Assets/Scripts/EscolherCordel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class EscolherCordel : MonoBehaviour {
+
+	public Button buttonCordelUm;
+	public Button buttonCordelDois;
+
+	// Use this for initialization
+	void Start () {
+		buttonCordelUm = buttonCordelUm.GetComponent<Button> ();	
+		buttonCordelDois = buttonCordelDois.GetComponent<Button> ();
+	}
+	
+	public void StartCordelUm(){
+		Application.LoadLevel ("tangram_1");
+	}
+
+	public void StartCordelDois(){
+		Application.LoadLevel ("EscolherCordel");
+	}
+}
